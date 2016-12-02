bdbc569ff9093678faee2ad5082043121045f489
Francisco Perez <f.perez475@gmail.com>
2016-12-02 13:42:31 -0800

    Moar

--- arduinoIDE/gui/gui.py
+++ arduinoIDE/gui/gui.py
@@ -149,15 +149,24 @@ class MainWindow(QMainWindow):
 
 		# ----------------------- END LEFT WIDGET ARDUINO MOTOR (Down) -------------------------
 
+		# ---------------------------------- RIGHT WIDGET  --------------------------------------
+
+		self.buttonAux = QPushButton("Switch")
+		self.buttonAux.clicked.connect(lambda:self.switchWidget())
+		self.rightLayout.addWidget(self.buttonAux)
+
+
+		# ---------------------------------- END RIGHT WIDGET  --------------------------------------
+
 		self.leftWidgetUp.setLayout(self.leftLayoutUp)
 		self.leftWidgetDown.setLayout(self.leftLayoutDown)
-		self.mainLayout.addWidget(self.leftWidgetUp)
-		self.mainLayout.addWidget(self.leftWidgetDown)
-		self.mainLayout.addLayout(self.rightLayout)
+		self.rightWidget.setLayout(self.rightLayout)
+		self.mainLayout.addWidget(self.leftWidgetUp, 0)
+		self.mainLayout.addWidget(self.leftWidgetDown, 0)
+		self.mainLayout.addWidget(self.rightWidget,1)
 
 		self.leftWidgetUp.setVisible(True)
-		self.leftWidgetDown.setVisible(True)
-
+		self.leftWidgetDown.setVisible(False)
 
 		'''signals'''
 		self.button_leds.clicked.connect(lambda:self.ledButtonClicked())
@@ -172,7 +181,6 @@ class MainWindow(QMainWindow):
 
 		self.setCentralWidget(self.horizontalLayoutWidget)
 		self.show()
- 
 
 	def initMenuBar(self):
 		
@@ -200,39 +208,90 @@ class MainWindow(QMainWindow):
 		fileMenu.addAction(newAction)
 		fileMenu.addAction(closeAction)
 
-		'''Examples menu'''
+		'''Examples menus and submenus'''
 		sub_control = QMenu('Arduino Control',self)
 		sub_motor = QMenu('Arduino Motor',self)
 		sub_behaviour = QMenu('Behaviours', self)	# comportamientos: sigue linea, sigue luz...
 
 		sub_sub_leds = QMenu('LEDs', self)
 		sub_sub_buzzer = QMenu('Buzzer', self)
-		sub_sub_ultrasound = QMenu('Ultrasound', self)
 		sub_sub_light = QMenu('Light sensors', self)
-		sub_sub_ir = QMenu('IR sensors', self)
-		sub_sub_motors = QMenu('Motors', self)
 		sub_sub_potentiometer = QMenu('Potentiometer', self)
 		sub_sub_lcd = QMenu('LCD', self)
 		sub_sub_keys = QMenu('Keys', self)
 
+		sub_sub_ultrasound = QMenu('Ultrasound', self)
+		sub_sub_ir = QMenu('IR sensors', self)
+		sub_sub_motors = QMenu('Motors', self)
 
+
+		# Examples Actions (code)
 		turn_on_led = QAction('Key Leds', self)
 		turn_on_led.setStatusTip('Turn on a specific LED pushing a key')
-		turn_on_led.triggered.connect(self.keys_led)
+		turn_on_led.triggered.connect(lambda:self.loadExample("keys_led"))
+		buzz_song = QAction('Play a Song', self)
+		buzz_song.setStatusTip('Play a song using the buzzer')
+		buzz_song.triggered.connect(lambda:self.loadExample("song_buzzer"))
 
+
+		ultra_distance = QAction('Read ultrasound', self)
+		ultra_distance.setStatusTip('Read values from ultrasonic sensor')
+		ultra_distance.triggered.connect(lambda:self.loadExample("ultra_distance"))
+
+		follow_line = QAction('Follow Line', self)
+		follow_line.setStatusTip('Follow a black line using IR sensors')
+		follow_line.triggered.connect(lambda:self.loadExample("follow_line"))
+
+		#
+		# Adding actions to submenus
+		#
+
+		# Arduino Control
+		sub_sub_leds.addAction(turn_on_led)
+		sub_sub_buzzer.addAction(buzz_song)
+
+		# Arduino Motor
+		sub_sub_ultrasound.addAction(ultra_distance)
+
+		# Behaviours
+		sub_behaviour.addAction(follow_line)
+
+
+		#
+		# Adding submenus to upper menus
+		#
+		sub_control.addMenu(sub_sub_leds)
+		sub_control.addMenu(sub_sub_buzzer)
+		sub_control.addMenu(sub_sub_light)
+		sub_control.addMenu(sub_sub_potentiometer)
+		sub_control.addMenu(sub_sub_lcd)
+		sub_control.addMenu(sub_sub_keys)
+		sub_motor.addMenu(sub_sub_motors)
+		sub_motor.addMenu(sub_sub_ir)
+		sub_motor.addMenu(sub_sub_ultrasound)
+		#sub_behaviour.addAction(None)
+
+		#
+		# Adding submenus to parent menu
+		#
 		exampleMenu = menubar.addMenu('&Examples')
 		exampleMenu.addMenu(sub_control)
 		exampleMenu.addMenu(sub_motor)
 		exampleMenu.addMenu(sub_behaviour)
 
-		sub_control.addAction(turn_on_led)
-		#sub_motor.addAction(None)
-		#sub_behaviour.addAction(None)
+		
+	def switchWidget(self):
+		if self.leftWidgetUp.isVisible():
+			self.leftWidgetUp.setVisible(False)
+			self.leftWidgetDown.setVisible(True)
+		else:
+			self.leftWidgetUp.setVisible(True)
+			self.leftWidgetDown.setVisible(False)
 
 
-
-	def keys_led(self):
-		print("Turn a led on")
+	'''LoadExamples'''
+	def loadExample(self, example_name):	
+		print("Loading example", example_name)
 
 	def openFile(self):
 		filename, _ = QFileDialog.getOpenFileName(self, 'Open File', os.getenv('HOME'))
